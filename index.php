ce4eb1c0ee441b274c2b9c93945b27a0365fe501
Spriinty <quentinpetrequin@gmail.com>
2019-12-20 09:22:07 +0000

    Push master modif csv

--- index.php
+++ index.php
@@ -5,11 +5,21 @@ require './vendor/autoload.php';
 
 $router = new AltoRouter();
 
-$router->map( 'GET', '/', 'render_home', 'home' );
+// map homepage
+$router->map( 'GET', '/', function() {
+	require __DIR__ . '/views/home.php';
+});
 
+// match current request url
 $match = $router->match();
 
-
+// call closure or throw 404 status
+if( is_array($match) && is_callable( $match['target'] ) ) {
+	call_user_func_array( $match['target'], $match['params'] ); 
+} else {
+	// no route was matched
+	header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
 
 // define('VIEW_PATH', dirname(__DIR__).'/views');
 
