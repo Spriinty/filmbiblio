8e3ecebbf44a8421a88f007b2a7a0f1866c7312c
florent <bonneau.florent44@gmail.com>
2020-01-02 14:58:29 +0000

    ajout bouton

--- model/movies.php
+++ model/movies.php
@@ -1,3 +1,4 @@
+   <pre><?php
 function test_input($data) {
     $data = trim($data);
     $data = stripslashes($data);
@@ -5,7 +6,7 @@ function test_input($data) {
     return $data;
     }
     try {
-        $base = new PDO('mysql:host=localhost; dbname=departement_france', 'root', '1234');
+        $base = new PDO('mysql:host=localhost; dbname=testcine', 'root', '1234');
     }
     catch(exception $e) {
         die('Erreur '.$e->getMessage());
@@ -14,7 +15,8 @@ function test_input($data) {
     $base->exec("SET CHARACTER SET utf8");
     // $retour = $base->query("SELECT * FROM `table_dep` WHERE `nom_maj_first` LIKE '%".$_POST['search']."%' OR `num` LIKE '%".$_POST['search']."%' OR `maj_nom_entier` LIKE '".$_POST['search']."%' OR `min_nom_entier` LIKE '%".$_POST['search']."%'");
     
-    $reponse1= $base->prepare("SELECT * FROM `table_dep` WHERE `nom_maj_first` LIKE '%".$_POST['search']."%' OR `num` LIKE '%".$_POST['search']."%' OR `maj_nom_entier` LIKE '".$_POST['search']."%' OR `min_nom_entier` LIKE '%".$_POST['search']."%'");
+    $reponse1= $base->prepare("SELECT * FROM `filmdescri` WHERE `COL 1` LIKE '%".$_POST['search']."%'");
+
     $reponse2= $base->prepare("SELECT * FROM `table_dep` WHERE `nom_maj_first` LIKE '%".$_POST['search']."%' OR `num` LIKE '%".$_POST['search']."%' OR `maj_nom_entier` LIKE '".$_POST['search']."%' OR `min_nom_entier` LIKE '%".$_POST['search']."%'");
     $reponse3= $base->prepare("SELECT * FROM `table_dep` WHERE `nom_maj_first` LIKE '%".$_POST['search']."%' OR `num` LIKE '%".$_POST['search']."%' OR `maj_nom_entier` LIKE '".$_POST['search']."%' OR `min_nom_entier` LIKE '%".$_POST['search']."%'");
 
@@ -30,18 +32,21 @@ function test_input($data) {
     $reponse2->execute();
     $reponse3->execute();
 
-    $allData1->$reponse1->fetchAll();
-    $allData2->$reponse2->fetchAll();
-    $allData3->$reponse3->fetchAll();
+    $allData1=$reponse1->fetchAll();
+    $allData2=$reponse2->fetchAll();
+    $allData3=$reponse3->fetchAll();
 
     $allData = array_merge($allData1,$allData2,$allData3);
-
+ 
+    print_r ($allData);
+   
      // var_dump(count($allData));
-     echo json_encode($allData);
-     // print_r($allData);
+    //  echo json_encode($allData);
+    //  var_dump($allData);
+    //  print_r($allData);
     // echo json_encode( $retour->fetchAll());
-        while ($data = $retour->fetch()){
-        echo '<tr><td>'.$data['num'].'</td><td>'.$data['maj_nom_entier'].'</td></tr>';
-        }
+        // while ($data = $retour->fetch()){
+        // echo $data['COL 1'];
+        // }
         $base = null;
-?>
+?> <pre>
--- views/home.php
+++ views/home.php
@@ -8,8 +8,68 @@
 </head>
 <body>
     <h1>test</h1>
-    <form action="">
-    <button type="submit">valider</button>
+    <form action="" id="form">
+    <input type="search" name="search" id="search">
+    <button type="submit">Suicide Squad</button>
+    <button type="submit">Diversion</button>
+    <button type="submit">Instinct de survie</button>
+    <button type="submit">Shaolin</button>
+    <button type="submit">Bievenue chez les Ch'tis</button>
+    <button type="submit">Chicken Run</button>
+    <button type="submit">Le Mans 66</button>
+    <button type="submit">Rush</button>
+    <button type="submit">Drive</button>
+    <button type="submit">Whiplash</button>
+    <button type="submit">Get Out</button>
+    <button type="submit">Parasite</button>
+    
     </form>
+    <div id="reponse"></div>
+
+<script>
+    // on selectionne le formulaire
+    const myForm = document.querySelector('#form');
+
+
+    // on affecte une ecoute d'évenement submit
+    myForm.addEventListener('keyup', function (e) {
+
+      //interrompre l'évenement par défaut
+      e.preventDefault();
+
+      //on selectionne la case de formulaire
+      nameInpt = document.querySelector('#search');
+
+
+      datas = new FormData(form);
+
+      datas.append('search', search.value);
+
+      fetch('../model/movies.php', {
+        method: 'POST',
+        body: datas
+      }).then(function (theResponse) {
+        return theResponse.text();
+      }).then(function (result) {
+        myDiv = document.querySelector('#reponse');
+        myDiv.innerHTML = result
+      })
+
+      // //on execute une promesse(ajax)
+      // fetch(`traitement.php?search=${nameInpt.value}`)
+      // .then(function(theresponse){
+      //     return theresponse.text();
+      // })
+      // .then(function(datas){
+
+
+      //     const myDiv = document.querySelector('#reponse');
+
+      //     //on met la reponse dans le paragraphe
+      //     myDiv.innerHTML = datas
+      // });
+
+    })
+    </script>
 </body>
 </html>
