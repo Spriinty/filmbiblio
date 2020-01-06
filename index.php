f666db9cd62592cfa7bc57f8dad05a2e06b45ad2
rayan-acs <rayan.sadki@protonmail.com>
2020-01-06 10:31:23 +0000

    Home Page Rayan

--- index.php
+++ index.php
@@ -11,9 +11,6 @@ if(isset($_SERVER['REQUEST_URI']) && !empty($_SERVER['REQUEST_URI'])){
         case '' :
             require __DIR__ . '/views/home.php';
             break;
-        case '/about' :
-            require __DIR__ . '/views/about.php';
-            break;
         default:
             http_response_code(404);
             require __DIR__ . '/views/404.php';
