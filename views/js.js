2e8597ec3877667ed4c3edc4ef8f80ca7bdbb78c
Spriinty <quentinpetrequin@gmail.com>
2020-01-07 11:19:29 +0000

    Push master function scrollTo

--- views/js.js
+++ views/js.js
@@ -3,7 +3,7 @@ let inptFormImg = document.querySelectorAll('.inpt-form-img');
 console.log(inptFormImg);
 
 inptFormImg.forEach(function(imageclic){
-    imageclic.addEventListener('click', function(clicimage){
-            window.scrollTo(0,document.body.scrollHeight);
+    imageclic.addEventListener('click', function(){
+            window.scrollTo(0, 190);
         });
     })
